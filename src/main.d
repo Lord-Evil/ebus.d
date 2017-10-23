12177cf7f6428af39950e82482f84e320dbe8a95
Lord_Evil <Lord_Evil@om-webmasters.org>
2017-10-24 03:00:12 +1000

    few more steps + dub.sdl

--- src/main.d
+++ src/main.d
@@ -6,6 +6,7 @@ import std.conv: to;
 import std.format;
 import std.file;
 import std.string;
+import std.algorithm;
 //Third-party Libs
 import vibe.data.json;
 import vibe.core.core : runApplication;
@@ -39,8 +40,76 @@ import vibe.http.websockets;
 */
 
 
+class BusGroup
+{
+	class Subscription
+	{
+	private:
+		WebSocket[string] subscribers;
+	public:
+		protected Json tags;
+		this(Json _tags){
+			tags=_tags;
+		}
+		void addSubscriber(WebSocket s,string seq){
+			if(seq !in subscribers)
+				subscribers[seq]=s;
+		}
+		void removeSubscriber(WebSocket s){
+			string[] keysToRemove;
+			foreach(string key,WebSocket sub;subscribers){
+				if(sub==s)
+					keysToRemove~=key;
+			}
+			foreach(string key;keysToRemove){
+				subscribers.remove(key);
+			}
+		}
+		void removeSubscriber(string seq){
+			subscribers.remove(seq);
+		}
+	}
+
+	immutable string name;
+	Subscription[] subs;//key is JSON array
+	this(string _name){
+		name=name;
+	}
+	void addSubscription(Json tags, WebSocket subscriber, string seq){
+		foreach(Subscription sub; subs){
+			bool fits=true;
+			if(tags.length!=sub.tags.length||tags.length==0)
+				continue;
+			foreach(Json tag;tags){
+				switch (tag.type) {
+					case Json.Type.string:
+
+						break;
+					case Json.Type.array:
+						break;
+					case Json.Type.object:
+						break;
+					case Json.Type.undefined:
+					case Json.Type.null_:
+					case Json.Type.bool_:
+					case Json.Type.int_:
+					case Json.Type.bigInt:
+					case Json.Type.float_:
+					default:
+						continue;//might need to remove bad tag
+				}
+			}
+		}
+	}
+
+}
+
+Json groups;
 
 void main(){
+	//can not initialize objects in global
+	groups = Json.emptyObject;
+
 	Json config=parseJsonString(cast(string)std.file.read("config.json"));
 	logInfo("Server started!");
 	auto router = new URLRouter;
@@ -58,20 +127,61 @@ void main(){
 	runApplication();
 }
 WebSocket[] m_socks;
-Json groups = Json.emptyObject;
 void handleConn(scope WebSocket sock)
 {
 	logInfo("Incomming connection! "~sock.request.clientAddress.to!string~" "~sock.request.headers["Sec-WebSocket-Key"]);
 	//logInfo(sock.request.headers);
 	m_socks~=sock;
 	while (sock.waitForData()) {
-		auto msg = sock.receiveText();
-		auto data = parseJsonString(msg);
+		string msg = sock.receiveText();
+		Json data;
+		try{
+			data = parseJsonString(msg);
+			writeln(data);
+		}catch(Exception e){
+			continue;
+		}
 
 		// TODO: add data in queue and do stuff in other place
+		if(data["group"].type!=Json.Type.undefined){
+			string group_name = data["group"].get!string;
+			if (groups[group_name].type==Json.Type.undefined){
+				groups[group_name] = Json.emptyObject;
+				writeln("Created new group "~group_name);
+			}else{
+				writeln("Existing group "~group_name);
+			}
+			if(data["data"]["action"].type!=Json.Type.undefined){
+				string action = data["data"]["action"].get!string;
+				switch(action){
+					case "join":
+						
+						break;
+					case "subscribe":
+						
+						break;
+					case "request":
+						
+						break;
+					case "chat":
+						
+						break;
 
-		auto group_name = data["group"].get!string;
-		if (groups[group_name]) groups[group_name] = Json.emptyObject;
+					case "invoke":
+						
+						break;
+					case "unsubscribe":
+						
+						break;
+					case "exit":
+						
+						break;
+					default:break;
+
+				}
+			}
+		}
+		/*
 		switch (data["data"]["action"].get!string)
 		{
 			case "join":
@@ -114,6 +224,7 @@ void handleConn(scope WebSocket sock)
 		//		if(s_sock!=sock)
 		//			s_sock.send(data.toString());
 		//}
+		*/
 	}
 	logInfo("Connection closed! "~sock.request.clientAddress.to!string~" "~sock.request.headers["Sec-WebSocket-Key"]);
 
