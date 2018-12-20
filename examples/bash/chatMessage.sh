5620edea3bbbf883cce4f296ca528819026b31ec
Lord_Evil <Lord_Evil@om-webmasters.org>
2018-12-20 05:44:39 +0000

    Improved bash script example to send user messages

--- examples/bash/chatMessage.sh
+++ examples/bash/chatMessage.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-wget "http://localhost:4445/push/chat/invoke" --quiet --header="Content-Type: application/json" --post-data='{"tags":"message","data":{"text":"Hello from Linux","from":"bash"}}' -O -
+if [ "$#" -lt 1 ]; then
+	wget "http://localhost:4445/push/chat/invoke" --quiet --header="Content-Type: application/json" --post-data='{"tags":"message","data":{"text":"Hello from Linux","from":"bash"}}' -O -
+else
+	msg='{"tags":"message","data":{"text":"'$@'","from":"bash"}}'
+	wget "http://localhost:4445/push/chat/invoke" --quiet --header="Content-Type: application/json" --post-data="$msg" -O -
+fi
 echo
