84e924d9ba657b24aea0971e04a247db69aa1d54
Lord_Evil <Lord_Evil@om-webmasters.org>
2019-11-18 15:37:21 +1000

    extra websocket handling

--- src/main.d
+++ src/main.d
@@ -95,108 +95,116 @@ void handleConn(scope WebSocket sock)
 	//writeln(sock.request.headers);
 	m_socks.insert(sock);
 	Bus.BSubscription[] m_subs;
-	while (sock.waitForData()) {
-		string msg = sock.receiveText();
-		Json data;
-		string seqID;
-		try{
-			/* Format: 
-				{
-					"group": <string name>,
-					"action": "subscribe/invoke/etc",
-					["seq": "ad19690109566ab3",]
-					["tags": Json,]
-					["data": Json]
-			*/
-			data = parseJsonString(msg);
-			writeln(data);
-			if(data["alive"].type!=Json.Type.undefined) continue;
-			seqID=data["seq"].get!string;
-		}catch(Exception e){
-			writeln("#####ERROR####");
-			writeln(e.msg);
-			writeln(msg);
-			writeln("##############");
-			continue;
-		}
+	try{
+		while (sock.waitForData()) {
+			string msg = sock.receiveText();
+			Json data;
+			string seqID;
+			try{
+				/* Format: 
+					{
+						"group": <string name>,
+						"action": "subscribe/invoke/etc",
+						["seq": "ad19690109566ab3",]
+						["tags": Json,]
+						["data": Json]
+				*/
+				data = parseJsonString(msg);
+				writeln(data);
+				if(data["alive"].type!=Json.Type.undefined) continue;
+				seqID=data["seq"].get!string;
+			}catch(Exception e){
+				writeln("#####ERROR####");
+				writeln(e.msg);
+				writeln(msg);
+				writeln("##############");
+				continue;
+			}
 
-		// TODO: add data in queue and do stuff in other place
-		if(data["group"].type!=Json.Type.undefined){
-			string group_name = data["group"].get!string;
-			if(config["auth"].get!bool){
-				string rname=resolveGroup(group_name,config["authUrl"].get!string,config["authKey"].get!string);
-				if(rname.length>1){
-					reGroup[rname]=group_name;
-					group_name=rname;
+			// TODO: add data in queue and do stuff in other place
+			if(data["group"].type!=Json.Type.undefined){
+				string group_name = data["group"].get!string;
+				if(config["auth"].get!bool){
+					string rname=resolveGroup(group_name,config["authUrl"].get!string,config["authKey"].get!string);
+					if(rname.length>1){
+						reGroup[rname]=group_name;
+						group_name=rname;
+					}
 				}
-			}
-			if (group_name !in groups){
-				groups[group_name] = new BGroup(group_name);
-				//writeln("Created new group "~group_name);
-			}else{
-				//writeln("Existing group "~group_name);
-			}
-			if(data["action"].type!=Json.Type.undefined){
-				string action = data["action"].get!string;
-				switch(action){
-					case "join":
-						//no real purpose, could be used for member count or auth (via tokens etc)
-						writeln("Join group "~group_name);
-						break;
-					case "subscribe":
-						if(data["tags"].type==Json.Type.undefined) break;
-						Json tags = data["tags"];
-						if(tags.length>0) {
-							m_subs~=groups[group_name].Subscribe(tags, sock, seqID);
-						}
-						writeln("Subscripe for tags "~tags.toString());
-						break;
-					case "request":
-						
-						break;
-					case "chat":
-						
-						break;
+				if (group_name !in groups){
+					groups[group_name] = new BGroup(group_name);
+					//writeln("Created new group "~group_name);
+				}else{
+					//writeln("Existing group "~group_name);
+				}
+				if(data["action"].type!=Json.Type.undefined){
+					string action = data["action"].get!string;
+					switch(action){
+						case "join":
+							//no real purpose, could be used for member count or auth (via tokens etc)
+							writeln("Join group "~group_name);
+							break;
+						case "subscribe":
+							if(data["tags"].type==Json.Type.undefined) break;
+							Json tags = data["tags"];
+							if(tags.length>0) {
+								m_subs~=groups[group_name].Subscribe(tags, sock, seqID);
+							}
+							writeln("Subscripe for tags "~tags.toString());
+							break;
+						case "request":
+							
+							break;
+						case "chat":
+							
+							break;
 
-					case "invoke":
-						if(data["tags"].type==Json.Type.undefined) break;
-						Json tags = data["tags"];
-						writeln("Invoke tags "~tags.toString());
-						auto subs=groups[group_name].findSubscriptionsForInvoke(tags);
-						if(subs.length < 1) break;
-						Json busMsg=Json.emptyObject;
-						if(config["auth"].get!bool){
-							string* group = (group_name in reGroup);
-							if(group!=null)
-								busMsg["group"]=*group;
-						}else{
-							busMsg["group"] = group_name;
-						}
-						busMsg["action"] = "invoke";
-						busMsg["event"] = Json.emptyObject;
-						busMsg["event"]["tags"]=tags;
-						if(data["data"].type != Json.Type.undefined)
-							busMsg["data"] = data["data"];
-						foreach(BSubscription sub; subs) {
-							foreach(string seq, WebSocket s; sub.subscribers) {
-								if(s!=sock){
-									busMsg["seqID"] = seq;
-									busMsg["event"]["matchedTags"]=deserializeTags(sub.tags);
-									s.send(busMsg.toString());
+						case "invoke":
+							if(data["tags"].type==Json.Type.undefined) break;
+							Json tags = data["tags"];
+							writeln("Invoke tags "~tags.toString());
+							auto subs=groups[group_name].findSubscriptionsForInvoke(tags);
+							if(subs.length < 1) break;
+							Json busMsg=Json.emptyObject;
+							if(config["auth"].get!bool){
+								string* group = (group_name in reGroup);
+								if(group!=null)
+									busMsg["group"]=*group;
+							}else{
+								busMsg["group"] = group_name;
+							}
+							busMsg["action"] = "invoke";
+							busMsg["event"] = Json.emptyObject;
+							busMsg["event"]["tags"]=tags;
+							if(data["data"].type != Json.Type.undefined)
+								busMsg["data"] = data["data"];
+							foreach(BSubscription sub; subs) {
+								foreach(string seq, WebSocket s; sub.subscribers) {
+									if(s!=sock){
+										busMsg["seqID"] = seq;
+										busMsg["event"]["matchedTags"]=deserializeTags(sub.tags);
+										try {
+											s.send(busMsg.toString());
+										} catch (Exception e){
+											writeln("Seems like websocket has died: "~s.request.clientAddress.to!string~" "~s.request.headers["Sec-WebSocket-Key"]);
+										}
+									}
 								}
 							}
-						}
-						break;
-					case "unsubscribe":
-						
-						break;
-					case "exit":
-						
-						break;
-					default:break;
+							break;
+						case "unsubscribe":
+							
+							break;
+						case "exit":
+							
+							break;
+						default:break;
+					}
 				}
 			}
 		}
+	}catch(Exception e){
+		writeln("!!!!!The most unexpected thing happened: "~e.msg);	
 	}
 	writeln("Connection closed! "~sock.request.clientAddress.to!string~" "~sock.request.headers["Sec-WebSocket-Key"]);
 	for(int i=0;i<m_subs.length;i++){
